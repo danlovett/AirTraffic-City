136d53a6cfb857136a04cb959a3b08e34a817d09
Dan Lovett <51034848+danlovett@users.noreply.github.com>
2022-09-23 21:35:16 +0100

    Design Change & improvements
    Worked on design of homepage.
    
    UI
     - Better UI background gradient animation
     - Images span across selective field
         |-> Subtle shade used to distinguish between text and image
     - Library where level selection occurs is now fully scrollable
    
    PAGES
     - Moved into 1 page: leaderboard and history screen now available in same page
     - updates to fetching info from JSON

--- pkg/processLibrary.js
+++ pkg/processLibrary.js
@@ -14,9 +14,45 @@ $.get('../pkg/JSON/info.json', function(data) {
             href: '#',
         }).appendTo('#levels');
         $('<h2>',{
-            text: index.library.levels.titles[i]
+            text: index.library.levels.titles[i],
+            class: "background-filter",
         }).appendTo(`#child${i}`)
 
         document.getElementById(`child${i}`).style.backgroundImage = index.library.levels.images[i]
+        
     }
 }, 'text');
+
+document.getElementById('nav-leaderboard').addEventListener('click', () => {
+    document.getElementById('leaderboard').style.display = 'block'
+    document.getElementById('library').style.filter = 'blur(8px)'
+    document.getElementById('library').style.pointerEvents = 'none'
+    document.getElementById('links').style.display = 'none'
+    document.getElementById('user').style.display = 'flex'
+
+})
+
+document.getElementById('close').addEventListener('click', () => {
+    document.getElementById('user').style.display = 'none'
+    document.getElementById('links').style.display = 'flex'
+    document.getElementById('library').style.pointerEvents = 'all'
+    document.getElementById('library').style.filter = 'blur(0px)'
+    document.getElementById('leaderboard').style.display = 'none'
+})
+
+document.getElementById('nav-history').addEventListener('click', () => {
+    document.getElementById('history').style.display = 'block'
+    document.getElementById('library').style.filter = 'blur(8px)'
+    document.getElementById('library').style.pointerEvents = 'none'
+    document.getElementById('links').style.display = 'none'
+    document.getElementById('user').style.display = 'flex'
+
+})
+
+document.getElementById('close').addEventListener('click', () => {
+    document.getElementById('user').style.display = 'none'
+    document.getElementById('links').style.display = 'flex'
+    document.getElementById('library').style.pointerEvents = 'all'
+    document.getElementById('library').style.filter = 'blur(0px)'
+    document.getElementById('history').style.display = 'none'
+})
