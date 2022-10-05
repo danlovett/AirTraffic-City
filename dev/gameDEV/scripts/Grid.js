7f833b523b2e412070e96e0b148f5111cbbe970c
Dan Lovett <51034848+danlovett@users.noreply.github.com>
2022-10-05 16:48:21 +0100

    Update 0.0.6.1
    Completed moving objects functionality
    Improved colours
    spawnable points are re-appended when an aircraft is no longer occupying the vector
    you can no longer move other aircraft whilst determining another's projections
    projection vector must be a neighbour of previous vector
    
    other
     - corrected link to play game where it used to direct user to an unfound directory

--- dev/gameDEV/scripts/Grid.js
+++ dev/gameDEV/scripts/Grid.js
@@ -3,10 +3,10 @@ class Grid {
         this.cols = cols;
         this.rows = rows;
         this.grid_size = size_of_cell;
-        this.space_occupied = {};
         this.moveable_areas = [[0,1], [2,1], [4,1], [6,1], [8,1], [10,1], [0,3], [2,3], [4,3], [6,3], [8,3], [10,3], [6,1],[0,2], [0,2], [0,2], [1,2], [2,2], [3,2], [4,2], [5,2], [6,2], [7,2], [8,2], [9,2], [10,2], [10, 3], [10,4], [10,5], [10,6]]
         this.spawn_areas = [[0,0], [2,0], [4,0], [6,0], [8,0], [10,0], [0,4], [2,4], [4,4], [6,4], [8,4]]
         this.callsign_prefixses = ['EZY', 'BAW', 'RYR', 'JBE', 'EZE', 'EJU', 'PJS', 'PRIV']
+        this.ac_types = ['A320', 'B738', 'B777', 'E145s']
     }
 
     render() {
@@ -27,24 +27,28 @@ class Grid {
         noStroke()
     }
 
-    show_areas(type) {
-        let area = undefined
-        switch (type) {
-            case 'move':
-                fill('red')
-                area = this.moveable_areas
-                break
-            case 'spawn':
-                fill('green')
-                area = this.spawn_areas
-                break
-            default:
-                area = this.moveable_areas
-
+    show_areas() {
+        for(let i = 0; i < planes.length; i++) {
+            if([planes[i].current_x, planes[i].current_y] != planes[i].spawn_point) {
+                this.spawn_areas.push(planes[i].spawn_point)
+            }
         }
-        for(let vector = 0; vector < area.length; vector++) {
-            // console.log(vector[0])
-            rect(area[vector][0] * this.grid_size, area[vector][1] * this.grid_size, this.grid_size, this.grid_size)
+        fill('grey')
+        for(let vector = 0; vector < this.moveable_areas.length; vector++) {
+            rect(this.moveable_areas[vector][0] * this.grid_size, this.moveable_areas[vector][1] * this.grid_size, this.grid_size, this.grid_size)
+        }
+        fill('blue')
+        for(let vector = 0; vector < this.spawn_areas.length; vector++) {
+            rect(this.spawn_areas[vector][0] * this.grid_size, this.spawn_areas[vector][1] * this.grid_size, this.grid_size, this.grid_size)
+        }
+    }
+
+    is_a_neighbour(x , y, current_p) {
+        let last_vector = planes[current_p].path_to_destination[planes[current_p].path_to_destination.length - 1]
+        if(last_vector[0] + 1 == x || last_vector[1] + 1 == y || last_vector[0] - 1 == x || last_vector[1] - 1 == y) {
+            return true
+        } else {
+            return false
         }
     }
 }
--- dev/gameDEV/scripts/script.js
+++ dev/gameDEV/scripts/script.js
@@ -5,49 +5,74 @@ let continue_travel_points = false
 let spawn = true
 let planes = []
 
+let permit_path = false
+let pathing_plane = undefined
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
+
     grid = new Grid(10, 10, grid_size)
-    for(let i = 0; i <= 4; i++) {
-        let spawn_point = grid.spawn_areas[floor(random(grid.spawn_areas.length))]
-        
-        planes.push(new Plane(spawn_point, 'A320'))
-        planes[i].makeCallsign()
-        
-        grid.spawn_areas.splice(grid.spawn_areas.indexOf(spawn_point), 1)
-    }
+
+    $.get('../../pkg/JSON/testLevel.json', function(data) {
+        for(let i = 0; i <= 4; i++) {
+            let spawn_point = data.spawnable_points[floor(random(data.spawnable_points.length))]
+            
+            planes.push(new Plane(spawn_point, 'A320'))
+            planes[i].makeCallsign()
+            data.spawnable_points.splice(data.spawnable_points.indexOf(spawn_point), 1)
+        }
+    })
+
+
+    // remove context menu to allow right clicks in browser
+	for (let element of document.getElementsByClassName("p5Canvas")) {
+    	element.addEventListener("contextmenu", (e) => e.preventDefault());
+  	}
+
+    frameRate(30)
 }
 
 function draw() {
     // clear()
-    background('grey')
+    background('green')
 
     // GRID
     grid_x = constrain(floor((mouseX/grid.grid_size)), 0, grid.cols)
     grid_y = constrain(floor((mouseY/grid.grid_size)), 0, grid.rows)
 
     grid.render()
-    grid.show_areas('move')
-    grid.show_areas('spawn')
+    grid.show_areas()
     
     //PLANES
     for(let i = 0; i < planes.length; i++) {
         planes[i].spawn()
+        planes[i].update_position()
+
         grid.render_selector_tool(grid_x, grid_y)
         planes[i].showCallsign()
-        // console.log(i, planes[i].current_x, planes[i].current_y)
+        if(planes[i].path_to_destination.length != 0 && permit_path == true) {
+            let path = planes[pathing_plane].path_to_destination[planes[i].path_to_destination.length - 1]
+            if([grid_x, grid_y] != path && grid.is_a_neighbour(grid_x, grid_y, pathing_plane)) {
+                planes[pathing_plane].update_travel_points([grid_x, grid_y])
+            } 
+        }
     }
 }
 
-function mouseClicked() {
-    console.log('click', grid_x, grid_y)
-}
-
-function mouseDragged() {
-    if((grid_x == plane.current_x && grid_y == plane.current_y) || continue_travel_points == true) {
-        plane.update_travel_points([grid_x, grid_y])
-        continue_travel_points = true
+function mousePressed() {
+    for(let i = 0; i < planes.length; i++) {
+        if(mouseButton == LEFT) {
+            if(grid_x == planes[i].current_x && grid_y == planes[i].current_y && !permit_path) {
+                planes[i].update_travel_points([grid_x, grid_y])
+                permit_path = true;
+                pathing_plane = i;
+            }
+        } else if (mouseButton == RIGHT) {
+            planes[i].enable_moving = true
+            permit_path = false
+            // console.log(planes[pathing_plane].enable_moving, permit_path)
+        }
     }
 }
 
