e460f376e00ca72bc6a262b30697937dd3cfcaf1
Dan Lovett <51034848+danlovett@users.noreply.github.com>
2022-10-21 12:35:12 +0100

    File management change (0.0.9.3)
    Complete change to file management for entire project
     - makes more sense and follows how other games are managed
    
    Full migration to jQuery usage. there are no longer any DOM usage

--- pkg/processIndex.js
+++
@@ -1,7 +0,0 @@
-
-$.get('../pkg/JSON/library.json', function(data) {
-    let index = JSON.parse(data)
-
-    document.getElementById('version').textContent = index.about.version
-    document.getElementById('author').textContent = index.about.author
-}, 'text');
